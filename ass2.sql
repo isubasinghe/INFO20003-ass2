c48d5706afc1fb7298f7010779f7dc9ecfb2a7fe
Isitha Subasinghe <isubasinghe@student.unimelb.edu.au>
2018-09-13 13:12:57 +1000

    Finished assignment 2

--- ass2.sql
+++ ass2.sql
@@ -28,6 +28,7 @@ Print their username and total travel time in hours, minutes and seconds.
  GROUP BY username
  ORDER BY spentTime DESC LIMIT 1;
 
+
  /* Print the usernames of customers who travel on concession (i.e. faretype=0)
     and took a journey in January 2018.
   */
@@ -77,7 +78,28 @@ To travel in 1 zone costs €3, 2 zones €5, 3 zones €8.
 For customers who do not travel on concession, calculate the total amount paid
 by each customer for their travel. Print their username and total costs in Euros.
  */
-/* TODO */
+SELECT username, SUM(journeyCost)as cost FROM (
+  (SELECT username,
+  CASE
+    WHEN startZone=2 AND endZone=2 AND lineIDEnd<>lineIDStart
+      THEN 8
+    WHEN startZone<>endZone
+      THEN 5
+    ELSE
+      3
+  END AS journeyCost FROM (
+  (SELECT journeyID, username, s.zone as startZone, lineID as lineIDStart FROM customer
+    INNER JOIN journey j on customer.customerID = j.customerID
+    INNER JOIN station s on j.startStationID = s.stationID
+    WHERE faretype='1') AS T_1
+  NATURAL JOIN
+  (SELECT journeyID, username, s.zone as endZone, lineID as lineIDEnd FROM customer
+    INNER JOIN journey j on customer.customerID = j.customerID
+    INNER JOIN station s on j.endStationID = s.stationID
+    WHERE faretype='1') as T_2
+  )
+  ) AS T_3
+) GROUP BY username;
 
 /*
 Print the usernames of customers who have travelled on all lines
@@ -99,19 +121,24 @@ SELECT username FROM (
   GROUP BY username
 ) AS T_2 WHERE lineCount = (SELECT COUNT(*) FROM line);
 
+
+
 /*
 For each journey, show its journey ID and how many stations it passed
 through (count the end station but not the start station). Order results by the
 number of stations the journey passed through, from most to least.
  */
-/* TODO */
+SELECT journeyID,
+  CASE
+    WHEN startLine=endLine
+      THEN ABS(endSequence-startSequence)
+    ELSE
+      ABS(startSequence+endSequence)
+  END AS stationCount FROM (
+  (SELECT journeyID, sequence as startSequence, lineID as startLine FROM journey
+  INNER JOIN station s on journey.startStationID = s.stationID) AS T_1
+  NATURAL JOIN
+  (SELECT journeyID, sequence as endSequence, lineID as endLine FROM journey
+  INNER JOIN station s on journey.endStationID = s.stationID) AS T_2
+) ORDER BY stationCount DESC;
 
-
-
-
-SELECT username, MAX(totalTravelTime) as totalTravelTime FROM (
-SELECT username,
-		TIMEDIFF(endTime, startTime) AS totalTravelTime
-FROM journey NATURAL JOIN customer) AS T
-group by username
-order by totalTravelTime;
